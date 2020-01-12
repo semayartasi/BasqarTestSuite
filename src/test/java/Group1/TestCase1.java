cb7c91f61bed6bc61614530e381968389c38b26f
Apps at Techno Study <apps@techno.study>
2020-01-12 18:54:25 -0500

    fix wrong import

--- src/test/java/Group1/TestCase1.java
+++ src/test/java/Group1/TestCase1.java
@@ -1,9 +1,7 @@
 package Group1;
 
-import com.sun.tracing.dtrace.ProviderAttributes;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
